19a01ae6e73bf4050761ab78f2938e9db5c5bca3
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-03-19 17:22:55 +0530

    fix: remove firebase cred

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,3 +1,10 @@
 export const __prod__ = process.env.NODE_ENV === "production";
+
+// cookie auth
 export const COOKIE_NAME = process.env.COOKIE_NAME || "-";
 export const COOKIE_TOKEN = process.env.COOKIE_TOKEN || "-";
+
+// firebase
+export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID!;
+export const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY!;
+export const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL!;
