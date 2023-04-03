2384299fef67711b2fe215e33f575d27e4341242
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-04-04 01:52:17 +0530

    feat: add channel card

--- src/pages/channels.tsx
+++ src/pages/channels.tsx
@@ -1,9 +1,66 @@
 import React from "react";
 import { getServerSideProps } from "src/lib/getServerSideProps";
+import { Button } from "src/ui/Button";
+import { ChannelCard } from "src/ui/ChannelCard";
+
+const channels = [
+  {
+    id: 1680552796811,
+    access: "public",
+    name: "Boring chat",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 1680552796811,
+    access: "private",
+    name: "Being Human",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 1680552796811,
+    access: "private",
+    name: "Wrong spelling",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 1680552796811,
+    access: "public",
+    name: "Long title",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 1680552796811,
+    access: "private",
+    name: "Boring chat",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 1680552796811,
+    access: "public",
+    name: "Boring chat",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 1680552796811,
+    access: "private",
+    name: "Boring chat",
+    desc: "Just, a boring chat nothing to show here Lorem ipsum dolor sit amet",
+  },
+] as any[];
 
 const Channels = () => {
   return (
-    <div className="flex-grow text-2xl font-bold bg-dark-900 c">Channels</div>
+    <div className="grid p-1 pb-6 md:p-4 bg-dark-900 md:grid-cols-2">
+      <div className="justify-between m-4 mt-8 md:col-span-2 f">
+        <h4>Channels</h4>
+        <Button className="px-8 rounded-md" btn="success">
+          Add channel
+        </Button>
+      </div>
+      {channels.map((channel, idx) => (
+        <ChannelCard key={idx} {...channel} />
+      ))}
+    </div>
   );
 };
 
