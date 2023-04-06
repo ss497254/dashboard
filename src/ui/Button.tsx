9bd7230aa9da903c0eb37595bb8f94282a91be84
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-04-06 20:43:24 +0530

    fix: icons import in button

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import * as Icons from "../icons";
 import { Spinner } from "./Spinner";
 
 const BtnSizes = {
@@ -23,7 +22,7 @@ export interface ButtonProps
   disabled?: boolean;
   btn?: keyof typeof ButtonType;
   iconSize?: number;
-  icon?: keyof typeof Icons;
+  icon?: React.ReactNode;
   className?: string;
   children?: React.ReactNode;
   Icon?: React.ReactNode;
@@ -65,140 +64,8 @@ export const Button: React.FC<ButtonProps> = ({
           <Spinner size={22} className="mx-auto" />
         </div>
       )}
-      {icon && Icons[icon]({ size: iconSize, className: "mr-3" })}
+      {icon}
       {children}
     </button>
   );
 };
-
-// export const Buttosn = forwardRef<RefHandle, ButtonProps>(
-//   (
-//     {
-//       block,
-//       className,
-//       children,
-//       danger,
-//       disabled = false,
-//       onClick,
-//       icon,
-//       iconRight,
-//       loading = false,
-//       loadingCentered = false,
-//       shadow = true,
-//       size = "tiny",
-//       style,
-//       type = "primary",
-//       htmlType = "button",
-//       ariaSelected,
-//       ariaControls,
-//       tabIndex,
-//       role,
-//       as,
-//       textAlign = "center",
-//       ...props
-//     }: ButtonProps,
-//     ref
-//   ) => {
-//     // button ref
-//     // const containerRef = useRef<HTMLElement>(null)
-//     const buttonRef = useRef<HTMLButtonElement>(null);
-
-//     useImperativeHandle(ref, () => ({
-//       button: () => {
-//         return buttonRef.current;
-//       },
-//     }));
-
-//     let __styles = styleHandler("button");
-
-//     // styles
-//     const showIcon = loading || icon;
-
-//     let classes = [__styles.base];
-//     let containerClasses = [__styles.container];
-
-//     classes.push(__styles.type[type]);
-
-//     if (block) {
-//       containerClasses.push(__styles.block);
-//       classes.push(__styles.block);
-//     }
-
-//     if (shadow && type !== "link" && type !== "text") {
-//       classes.push(__styles.shadow);
-//     }
-
-//     if (size) {
-//       classes.push(__styles.size[size]);
-//     }
-
-//     if (className) {
-//       classes.push(className);
-//     }
-
-//     if (disabled) {
-//       classes.push(__styles.disabled);
-//     }
-
-//     const iconLoaderClasses = [__styles.loading];
-
-//     // custom button tag
-//     const CustomButton = ({ ...props }) => {
-//       const Tag = as as keyof JSX.IntrinsicElements;
-//       return <Tag {...props} />;
-//     };
-
-//     const buttonContent = (
-//       <>
-//         {showIcon &&
-//           (loading ? (
-//             <IconLoader size={size} className={iconLoaderClasses.join(" ")} />
-//           ) : icon ? (
-//             <IconContext.Provider value={{ contextSize: size }}>
-//               {icon}
-//             </IconContext.Provider>
-//           ) : null)}
-//         {children && <span className={__styles.label}>{children}</span>}
-//         {iconRight && !loading && (
-//           <IconContext.Provider value={{ contextSize: size }}>
-//             {iconRight}
-//           </IconContext.Provider>
-//         )}
-//       </>
-//     );
-
-//     if (as) {
-//       return (
-//         <CustomButton
-//           {...props}
-//           className={classes.join(" ")}
-//           onClick={onClick}
-//           style={style}
-//         >
-//           {buttonContent}
-//         </CustomButton>
-//       );
-//     } else {
-//       return (
-//         // <span ref={containerRef} className={containerClasses.join(' ')}>
-//         <button
-//           {...props}
-//           ref={buttonRef}
-//           className={classes.join(" ")}
-//           disabled={loading || (disabled && true)}
-//           onClick={onClick}
-//           style={style}
-//           type={htmlType}
-//           aria-selected={ariaSelected}
-//           aria-controls={ariaControls}
-//           tabIndex={tabIndex}
-//           role={role}
-//           form={props.form}
-//         >
-//           {buttonContent}
-//         </button>
-//         // </span>
-//       );
-//     }
-//   }
-// );
