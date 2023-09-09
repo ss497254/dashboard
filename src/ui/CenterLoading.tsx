94a22caf31e036e29abb28f0acc23fd4a6546682
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-10 00:54:47 +0530

    update: center loading

--- src/ui/CenterLoading.tsx
+++ src/ui/CenterLoading.tsx
@@ -4,10 +4,10 @@ interface props {}
 
 export const CenterLoading: React.FC<props> = () => {
   return (
-    <div className="flex-col w-screen h-screen bg-white center">
-      <img src="/assets/icon.png" height={180} width={180} />
-      <div className="w-[160px] relative bg-blue-100 h-[3px] overflow-hidden my-8">
-        <div className="bg-blue-500 loading-bar w-16 h-[3px]"></div>
+    <div className="flex-col h-screen c">
+      <img src="/logo.png" height={180} width={180} />
+      <div className="w-[160px] relative bg-dark-700 h-[3px] overflow-hidden my-8">
+        <div className="bg-emerald-600 loading-bar w-16 h-[3px]"></div>
       </div>
       <div className="h-20" />
     </div>
