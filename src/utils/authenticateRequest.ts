13f0e2de47907a1f68f804cad1400b44ffdb9166
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-09 16:45:12 +0530

    remove unused utils functions

--- src/utils/authenticateRequest.ts
+++
@@ -1,7 +0,0 @@
-import { COOKIE_NAME, COOKIE_TOKEN } from "src/data/constants";
-
-export const authenticateRequest = ({ cookies }: any) => {
-  const cookie = cookies[COOKIE_NAME];
-
-  return cookie === COOKIE_TOKEN;
-};
