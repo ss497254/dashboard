f5c4544a83f7ac1109262b18d7d18703849860e5
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-09 16:59:18 +0530

    feat: add getMessageStore

---
+++ src/lib/getMessageStore.ts
@@ -0,0 +1,62 @@
+import { StoreApi, UseBoundStore, create } from "zustand";
+import { IMessage } from "src/types/IMessage";
+
+export interface IMessageStore {
+  newMessages: IMessage[];
+  oldMessages: IMessage[];
+  channel: string;
+  isLoading: boolean;
+  isSubmitting: boolean;
+  addMessage: (content: string) => Promise<void>;
+  loadOldMessages: () => Promise<void>;
+  clearMessages: () => void;
+}
+
+const MessageStoreMap = new Map<
+  string,
+  UseBoundStore<StoreApi<IMessageStore>>
+>();
+
+export const getMessageStore = (channel: string) => {
+  if (MessageStoreMap.has(channel)) return MessageStoreMap.get(channel)!;
+
+  const store = create<IMessageStore>()((set) => ({
+    newMessages: [],
+    channel,
+    oldMessages: [],
+    isLoading: false,
+    isSubmitting: false,
+
+    addMessage: async (content) => {
+      set({ isSubmitting: true });
+      const newMessage: IMessage = {
+        username: "ss497254",
+        content,
+        timestamp: new Date().getTime(),
+      };
+      await new Promise((res) => setTimeout(res, 2000));
+      set((state) => ({
+        newMessages: [...state.newMessages, newMessage],
+        isSubmitting: false,
+      }));
+    },
+
+    loadOldMessages: async () => {
+      set({ isLoading: true });
+      await new Promise((res) => setTimeout(res, 2000));
+      set({ isLoading: false });
+    },
+
+    clearMessages: () =>
+      set({
+        newMessages: [],
+        oldMessages: [],
+        isSubmitting: false,
+        isLoading: false,
+      }),
+  }));
+
+  MessageStoreMap.set(channel, store);
+
+  return store;
+};
