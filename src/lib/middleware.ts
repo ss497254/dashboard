1dc4cbf9727dae8dc60033c0f93b63c7f24c660a
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-09 16:50:21 +0530

    remove firebase and middleware

--- src/lib/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { authenticateRequest } from "src/utils/authenticateRequest";
-
-export const middleware =
-  (cb: any) => (req: NextApiRequest, res: NextApiResponse) => {
-    if (authenticateRequest(req)) return cb(req, res);
-
-    return res.status(403).json({ message: "nahi hai kuch" });
-  };
