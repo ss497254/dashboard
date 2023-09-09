8a0dde42febb8414bcca898b210ecc7c3c20264b
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-09 17:23:10 +0530

    update: add auth check in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import NProgress from "nprogress";
 import { Head } from "src/components/Head";
 import { ToastContainer } from "src/components/ToastContainer";
 import { Layout } from "src/components/layout";
+import { useConfigStore } from "src/stores/useConfigStore";
 import "src/styles/fonts.css";
 import "src/styles/globals.css";
 import "src/styles/loading-animation.css";
@@ -13,6 +14,7 @@ import "src/styles/nprogress.css";
 import "src/styles/scrollbar.css";
 import "src/styles/toast.css";
 import { PageComponent } from "src/types/PageComponent";
+import Error404 from "./404";
 
 Router.events.on("routeChangeStart", NProgress.start);
 Router.events.on("routeChangeComplete", NProgress.done);
@@ -22,17 +24,29 @@ function App({
   Component,
   pageProps,
 }: AppProps & { Component: PageComponent<NextPageContext> }) {
-  return (
-    <>
-      <Head />
-      {Component.auth ? (
+  const user = useConfigStore((state) => state.user);
+
+  if (Component.auth) {
+    if (!user) {
+      Router.replace("/login");
+      return <Error404 />;
+    }
+
+    return (
+      <>
+        <Head />
         <Layout>
           <ToastContainer />
           <Component {...pageProps} />
         </Layout>
-      ) : (
-        <Component {...pageProps} />
-      )}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
     </>
   );
 }
